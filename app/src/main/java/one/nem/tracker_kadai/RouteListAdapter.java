1afca5fc43fa8cd908e8a93f9727ffe410e25cc8
ろむねこ <nem@nem.x0.to>
2023-01-15 14:43:22 +0900

    アダプタ作ったりしてみた

---
+++ app/src/main/java/one/nem/tracker_kadai/RouteListAdapter.java
@@ -0,0 +1,49 @@
+package one.nem.tracker_kadai;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RouteListAdapter extends RecyclerView.Adapter<RouteListAdapter.RouteListViewHolder> {
+
+    private List<RowData> rowDataList;
+
+    public RouteListAdapter(List<RowData> rowDataList) {
+        this.rowDataList = rowDataList;
+    }
+
+    static class RouteListViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titleText;
+
+        RouteListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleText = itemView.findViewById(R.id.select_route_route_name);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RouteListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_select_route, parent, false);
+        return new RouteListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RouteListViewHolder holder, int position) {
+        holder.titleText.setText(rowDataList.testTitle);
+    }
+
+    @Override
+    public int getItemCount() {
+        return rowDataList.size();
+    }
+
+}
