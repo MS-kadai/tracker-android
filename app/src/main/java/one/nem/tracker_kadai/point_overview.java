2a4aa6ae8946c4c5c54ce950c1d29f0d71a497c9
ろむねこ <nem@nem.x0.to>
2023-01-22 00:42:05 +0900

    地点の概要見るためのページ（？）をつくりかけた

---
+++ app/src/main/java/one/nem/tracker_kadai/point_overview.java
@@ -0,0 +1,24 @@
+package one.nem.tracker_kadai;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class point_overview extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_point_overview, container, false);
+        return view;
+}
